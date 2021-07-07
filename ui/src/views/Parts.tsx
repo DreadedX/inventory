aae10d3999fe03208b18cefa4077e74821be6262
Dreaded_X <tim.huizinga@gmail.com>
2021-07-07 03:24:32 +0200

    Implemented parts search in the front-end

--- ui/src/views/Parts.tsx
+++ ui/src/views/Parts.tsx
@@ -1,6 +1,6 @@
-import { FC, useState, useEffect, MutableRefObject } from 'react';
-import { Link } from 'react-router-dom';
-import { Container, Button } from 'semantic-ui-react';
+import { FC, useState, useEffect, MutableRefObject, ChangeEvent } from 'react';
+import { Link, useLocation, useHistory } from 'react-router-dom';
+import { Container, Button, Input, Grid } from 'semantic-ui-react';
 import { PartList, LoadingBox, StatusBox } from '../components';
 import { request } from '../request';
 
@@ -8,36 +8,55 @@ interface Props {
 	ws: MutableRefObject<WebSocket | undefined>
 }
 
+const useQuery = () => {
+	return new URLSearchParams(useLocation().search)
+}
+
 export const Parts: FC<Props> = ( { ws }: Props ) => {
+	const query = useQuery();
+	const history = useHistory();
+
 	const [parts, setParts] = useState<ApiPart[]>([]);
-	const [loading, setLoading] = useState<boolean>(true);
+	const [ search, setSearch ] = useState(query.get("search") || "");
+	const [ loading, setLoading ] = useState<boolean>(true);
+	const [ searching, setSearching ] = useState<boolean>(false);
 	const [status, setStatus] = useState<JSX.Element>();
 
-	const update = () => {
-		request<ApiPart[]>("v1/part/list")
+	const update = (search: string) => {
+		request<ApiPart[]>("v1/part/list/" + search)
 			.then(response => {
 				if (response.data) {
 					setStatus(undefined);
-					setParts(response.data)
+					setParts(response.data);
 				} else if (response.message) {
 					setStatus(<StatusBox icon="question" message={ response.message }/>)
 				}
 
 				setLoading(false);
+				setSearching(false);
 			}).catch(error => {
 				console.error(error)
 				setStatus(<StatusBox icon="times" message={ error.message }/>)
 				setLoading(false);
+				setSearching(false);
 			});
 	}
 
 	useEffect(() => {
-		update()
-	}, []);
+		update(search)
+	}, [search]);
+
+	const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+		setSearching(true);
+		setSearch(event.target.value);
+		history.replace({search: event.target.value ? "?search="+event.target.value : ""})
+	}
 
 	useEffect(() => {
 		if (ws.current)  {
-			ws.current.onmessage = update;
+			ws.current.onmessage = () => {
+				update(search);
+			}
 		}
 
 		return () => {
@@ -45,14 +64,17 @@ export const Parts: FC<Props> = ( { ws }: Props ) => {
 				ws.current.onmessage = null;
 			}
 		}
-	}, [ws]);
+	}, [ws, search]);
 
 	return (
 			<Container style={{ margin: "3em" }}>
+				<Grid>
+					<Grid.Column width={14}><Input style={{ width: '100%' }} icon="search" loading={searching} type="search" value={search} placeholder="Search..." onChange={handleSearch}/></Grid.Column>
+					<Grid.Column width={2}><Button style={{ height: '100%' }} basic icon="add" as={Link} to="/part/create" floated="right" /></Grid.Column>
+				</Grid>
 				{ status || <LoadingBox loading={ loading }>
 					<PartList parts={ parts } />
 				</LoadingBox>}
-				<Button basic icon="add" as={Link} to="/part/create" floated="right" />
 			</Container>
 	);
 };
