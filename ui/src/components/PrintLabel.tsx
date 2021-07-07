b19c667ea4c6bf722db91b20647ee0fdc646a06f
Dreaded_X <tim.huizinga@gmail.com>
2021-07-08 01:14:55 +0200

    Improved print diaglog

--- ui/src/components/PrintLabel.tsx
+++ ui/src/components/PrintLabel.tsx
@@ -1,5 +1,5 @@
 import { FC, useState } from 'react';
-import { Modal, Button } from 'semantic-ui-react';
+import { Modal, Button, Message } from 'semantic-ui-react';
 import { requestStatus } from '../request';
 
 interface Props {
@@ -10,21 +10,27 @@ interface Props {
 export const PrintLabel: FC<Props & Record<string, any>> = ({ id, type, ...props }: Props) => {
 	const [ open, setOpen ] = useState(false);
 	const [ previewOpen, setPreviewOpen ] = useState(false);
+	const [ status, setStatus ] = useState<JSX.Element>();
 
 	const print = () => {
 		requestStatus("/v1/label/" + type + "/" + id, {method: "GET"})
 			.then(response => {
-				console.log(response)
+				console.log(response);
+				setOpen(false);
 			})
 			.catch(error => {
-				console.log(error)
+				console.log(error);
+				setStatus(<Message error>Unable to print!</Message>);
 			})
 	}
 
 	const trigger = <Button content="Preview" color="blue" />;
 
 	return (<Modal
-		onClose={() => setOpen(false)}
+		onClose={() => {
+			setOpen(false)
+			setStatus(undefined)
+		}}
 		onOpen={() => setOpen(true)}
 		open={open}
 		{...props}
@@ -33,15 +39,15 @@ export const PrintLabel: FC<Props & Record<string, any>> = ({ id, type, ...props
 			Print
 		</Modal.Header>
 		<Modal.Content>
-			Do you want to print out a label?
+			{ status || "Do you want to print out a label?" }
 		</Modal.Content>
 		<Modal.Actions>
 			<Button content="Cancel" color="black" onClick={() => setOpen(false)} />
+			{/* @todo Add closeIcon in the proper place*/}
 			<Modal basic onClose={() => setPreviewOpen(false)} onOpen={() => setPreviewOpen(true)} open={previewOpen} trigger={trigger}>
-				{/* @todo Center the image and resize properly on mobile */}
-				<img src={"/v1/label/" + type + "/" + id + "/preview"} alt="Preview of the label" />
+				<img width="100%" src={"/v1/label/" + type + "/" + id + "/preview"} alt="Preview of the label" />
 			</Modal>
-			<Button content="Print" color="green" icon="print" onClick={print} />
+			<Button disabled={status ? true : false} content="Print" color="green" icon="print" onClick={print} />
 		</Modal.Actions>
 	</Modal>)
 }
