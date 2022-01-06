5799904eee3821d08b3f39edbc3a919a52beaac6
Dreaded_X <tim.huizinga@gmail.com>
2022-01-06 22:38:58 +0100

    Removed old ui

--- ui.old/src/views/index.ts
+++
@@ -1,8 +0,0 @@
-export { Scan } from "./Scan";
-export { Parts } from "./Parts";
-export { Part } from "./Part";
-export { PartCreate } from "./PartCreate";
-export { Storages } from "./Storages";
-export { Storage } from "./Storage";
-export { StorageCreate } from "./StorageCreate";
-export { NotFound } from "./NotFound";
